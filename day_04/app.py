e7fb63e2c35b4c4cd7f2473d5a2a1bde97ee0bdb
Jennifer Moore <jenniferplusplus@gmail.com>
2019-12-08 22:26:11 -0800

    day 04, part 2

--- day_04/app.py
+++ day_04/app.py
@@ -1,13 +1,19 @@
-from day_04.lib import valid_pairs, valid_sequence
+from day_04.lib import valid_pairs, valid_sequence, valid_pairs_v2
 
 input = [153517, 630395]
 test_value = input[0]
 result = []
+result_v2 = []
 
 while test_value < input[1]:
     if valid_sequence(test_value) and valid_pairs(test_value):
         result.append(test_value)
+    if valid_sequence(test_value) and valid_pairs_v2(test_value):
+        result_v2.append(test_value)
     test_value += 1
 
 print(result)
 print(f'found {len(result)} possible combinations')
+
+print(result_v2)
+print(f'found {len(result_v2)} possible combinations with the revised criteria')
--- day_04/lib.py
+++ day_04/lib.py
@@ -17,3 +17,35 @@ def valid_pairs(val: int) -> bool:
             return True
     return False
 
+
+def valid_pairs_v2(val: int) -> bool:
+    digits = list(str(val))
+    i = 0
+    while i < len(digits):
+        count = duplicate_len(digits[i:])
+        if count == 2:
+            return True
+        i += count
+    return False
+
+
+def duplicate_len(digits: []) -> int:
+    position = 0
+    length = 1
+    prev = None
+
+    for d in digits:
+        if prev == None:
+            prev = d
+            continue
+        if prev == d:
+            length += 1
+        else:
+            break
+    # while digits[position] == digits[position + 1]:
+    #     position += 1
+    #     length += 1
+    #     if position + 1 >= len(digits):
+    #         break
+
+    return length
